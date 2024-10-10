992581fc98ea830283218f9b99da763de566f43d
perplexing714 <ryanyang1414@gmail.com>
2024-10-10 14:07:48 -0700

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,3 @@
+from flask import Flask, url_for, render_template, request
+
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
