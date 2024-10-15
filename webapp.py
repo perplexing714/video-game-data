8e37cc4bc8c21a867308644654801235e85cadc9
unknown <ryanyang1414@gmail.com>
2024-10-15 12:28:11 -0700

    test

--- webapp.py
+++ webapp.py
@@ -1,3 +1,10 @@
 from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+@app.route("/home")
+def render_main():
+    return render_template('home.html')
+    
+if __name__=="__main__":
+    app.run(debug=True)
